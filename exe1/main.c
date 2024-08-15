705ea8b7d3a88761d8c42fbc5799c2b20096285f
LucasLeviVaz <103457760+LucasLeviVaz@users.noreply.github.com>
2024-08-15 20:39:09 -0300

    ex1

--- exe1/main.c
+++ exe1/main.c
@@ -1,20 +1,26 @@
-#include "hardware/gpio.h"
-#include "pico/stdlib.h"
 #include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
 
-const int LED_PIN_R = 5;
+const int LED_PIN_V = 5;
+const int LED_PIN_A = 28;
 
 int main() {
-  stdio_init_all();
+    stdio_init_all();
 
-  gpio_init(LED_PIN_R);
-  gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_init(LED_PIN_V);
+    gpio_set_dir(LED_PIN_V, GPIO_OUT);
 
-  while (true) {
-    gpio_put(LED_PIN_R, 1);
-    sleep_ms(250);
+    gpio_init(LED_PIN_A);
+    gpio_set_dir(LED_PIN_A, GPIO_OUT);
 
-    gpio_put(LED_PIN_R, 0);
-    sleep_ms(250);
-  }
+    while (true) {
+        gpio_put(LED_PIN_V, 1);
+        gpio_put(LED_PIN_A, 1);
+        sleep_ms(250);
+
+        gpio_put(LED_PIN_V, 0);
+        gpio_put(LED_PIN_A, 0);
+        sleep_ms(250);
+    }
 }
